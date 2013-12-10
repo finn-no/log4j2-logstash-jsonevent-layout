c770d93b47b12198fa1ecc254b7d621adcc0d191
firojoha <roar@finn.no>
2013-12-10 08:53:34 +0100

    IAD-56556 IAD-56926 Teste greenpages-server med log4j2

--- src/main/java/net/logstash/logging/event/LogStashEvent.java
+++ src/main/java/net/logstash/logging/event/LogStashEvent.java
@@ -18,148 +18,148 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 //TODO order explicitly all nodes
 public abstract class LogStashEvent {
 
-	/**
-	 * @source: The source of the event which includes the plugin that generated
-	 *          it and the hostname that produced it.
-	 */
-	protected String source = null;
+    /**
+     * @source: The source of the event which includes the plugin that generated
+     *          it and the hostname that produced it.
+     */
+    protected String source = null;
 
-	/**
-	 * @tags: Tags on the event.
-	 */
-	protected List<String> tags = new ArrayList<String>();
+    /**
+     * @tags: Tags on the event.
+     */
+    protected List<String> tags = new ArrayList<String>();
 
-	/**
-	 * @fields: A set of fields.
-	 */
-	protected Map<String, Object> fields = new LinkedHashMap<String, Object>();
+    /**
+     * @fields: A set of fields.
+     */
+    protected Map<String, Object> fields = new LinkedHashMap<String, Object>();
 
-	/**
-	 * @timestamp: An ISO8601 timestamp.
-	 */
-	protected String timestamp = null;
+    /**
+     * @timestamp: An ISO8601 timestamp.
+     */
+    protected String timestamp = null;
 
-	/**
-	 * @source_host: Host of the event.
-	 */
-	protected String source_host = null;
+    /**
+     * @source_host: Host of the event.
+     */
+    protected String source_host = null;
 
-	/**
-	 * @source_path: Path of source
-	 */
-	protected String source_path = null;
+    /**
+     * @source_path: Path of source
+     */
+    protected String source_path = null;
 
-	/**
-	 * @message: The message
-	 */
-	protected String message = null;
+    /**
+     * @message: The message
+     */
+    protected String message = null;
 
-	/**
-	 * @type: The value of the type configuration option we set.
-	 */
-	protected String type = null;
+    /**
+     * @type: The value of the type configuration option we set.
+     */
+    protected String type = null;
 
-	@JsonProperty("@source")
-	public String getSource() {
-		return source;
-	}
+    @JsonProperty("@source")
+    public String getSource() {
+        return source;
+    }
 
-	protected void setSource(String source) {
-		this.source = source;
-	}
+    protected void setSource(String source) {
+        this.source = source;
+    }
 
-	@JsonProperty("@tags")
-	public List<String> getTags() {
-		return tags;
-	}
+    @JsonProperty("@tags")
+    public List<String> getTags() {
+        return tags;
+    }
 
-	protected void addTags(String tag) {
-		addTags(tag);
-	}
+    protected void addTags(String tag) {
+        addTags(tag);
+    }
 
-	protected void addTags(String... tags) {
-		if (tags != null && tags.length > 0) {
-			this.tags.addAll(Arrays.asList(tags));
-		}
-	}
+    protected void addTags(String... tags) {
+        if (tags != null && tags.length > 0) {
+            this.tags.addAll(Arrays.asList(tags));
+        }
+    }
 
-	public interface LOGSTASH_EVENT_JSON_KEYS {
-		public final static String FIELDS = "@fields";		
-	}
-	@JsonProperty("@fields")
-	public Map<String, Object> getFields() {
-		return fields;
-	}
+    public interface LOGSTASH_EVENT_JSON_KEYS {
+        public final static String FIELDS = "@fields";
+    }
+    @JsonProperty("@fields")
+    public Map<String, Object> getFields() {
+        return fields;
+    }
 
-	protected void addFields(String key, Object value) {
-		this.fields.put(key, value);
-	}
+    protected void addFields(String key, Object value) {
+        this.fields.put(key, value);
+    }
 
-	@JsonProperty("@timestamp")
-	public String getTimestamp() {
-		return timestamp;
-	}
+    @JsonProperty("@timestamp")
+    public String getTimestamp() {
+        return timestamp;
+    }
 
-	protected void setTimestamp(String timestamp) {
-		this.timestamp = timestamp;
-	}
+    protected void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 
-	@JsonProperty("@source_host")
-	public String getSource_host() {
-		return source_host;
-	}
+    @JsonProperty("@source_host")
+    public String getSource_host() {
+        return source_host;
+    }
 
-	protected void setSource_host(String source_host) {
-		this.source_host = source_host;
-	}
+    protected void setSource_host(String source_host) {
+        this.source_host = source_host;
+    }
 
-	@JsonProperty("@source_path")
-	public String getSource_path() {
-		return source_path;
-	}
+    @JsonProperty("@source_path")
+    public String getSource_path() {
+        return source_path;
+    }
 
-	protected void setSource_path(String source_path) {
-		this.source_path = source_path;
-	}
+    protected void setSource_path(String source_path) {
+        this.source_path = source_path;
+    }
 
-	@JsonProperty("@message")
-	public String getMessage() {
-		return message;
-	}
+    @JsonProperty("@message")
+    public String getMessage() {
+        return message;
+    }
 
-	protected void setMessage(String message) {
-		this.message = message;
-	}
+    protected void setMessage(String message) {
+        this.message = message;
+    }
 
-	@JsonProperty("@type")
-	public String getType() {
-		return type;
-	}
+    @JsonProperty("@type")
+    public String getType() {
+        return type;
+    }
 
-	protected void setType(String type) {
-		this.type = type;
-	}
-	
-	public static final String LOG_STASH_ISO8601_TIMESTAMP_FORMAT ="yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+    protected void setType(String type) {
+        this.type = type;
+    }
+
+    public static final String LOG_STASH_ISO8601_TIMESTAMP_FORMAT ="yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
 
 
-	private static ObjectMapper mapper; 
-	static {
-		mapper = new ObjectMapper();
-		mapper.setDateFormat(new SimpleDateFormat(LOG_STASH_ISO8601_TIMESTAMP_FORMAT));//Ahoy, X introduced in java 7
-		mapper.configure(Feature.ESCAPE_NON_ASCII, true);
-	}
-	
-	public static String marshallToJSON(LogStashEvent event) throws IOException {
-		return marshallToJSON(event, false);
-	}
+    private static ObjectMapper mapper;
+    static {
+        mapper = new ObjectMapper();
+        mapper.setDateFormat(new SimpleDateFormat(LOG_STASH_ISO8601_TIMESTAMP_FORMAT));//Ahoy, X introduced in java 7
+        mapper.configure(Feature.ESCAPE_NON_ASCII, true);
+    }
 
-	public static String marshallToJSON(LogStashEvent event, boolean prettyPrint) throws IOException {
-		if (prettyPrint) {
-			return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(event);
-		} else {
-			return mapper.writeValueAsString(event);
-		}
-	}
-	
+    public static String marshallToJSON(LogStashEvent event) throws IOException {
+        return marshallToJSON(event, false);
+    }
+
+    public static String marshallToJSON(LogStashEvent event, boolean prettyPrint) throws IOException {
+        if (prettyPrint) {
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(event);
+        } else {
+            return mapper.writeValueAsString(event) + '\n';
+        }
+    }
+
 }
--- src/test/java/net/logstash/logging/log4j2/core/layout/LogStashEventJSONLayoutIT.java
+++ src/test/java/net/logstash/logging/log4j2/core/layout/LogStashEventJSONLayoutIT.java
@@ -1,201 +1,193 @@
 package net.logstash.logging.log4j2.core.layout;
 
 import java.io.IOException;
-import java.util.ArrayList;
-
-import net.logstash.logging.event.LogStashEvent;
-import static net.logstash.logging.event.LogStashEvent.LOGSTASH_EVENT_JSON_KEYS;
-
-import org.apache.commons.collections.IteratorUtils;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.core.impl.Log4jLogEvent;
-
-import org.apache.logging.log4j.message.Message;
-import org.apache.logging.log4j.message.SimpleMessage;
-import org.hamcrest.Matcher;
-import org.testng.annotations.Test;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.collections.IteratorUtils;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.impl.Log4jLogEvent;
+import org.apache.logging.log4j.message.Message;
+import org.apache.logging.log4j.message.SimpleMessage;
+import org.hamcrest.Matcher;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-import static org.hamcrest.MatcherAssert.*;
+import static net.logstash.logging.event.LogStashEvent.LOGSTASH_EVENT_JSON_KEYS;
 import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.collection.IsIterableContainingInAnyOrder.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LogStashEventJSONLayoutIT {
-	private static Logger logger = LogManager.getLogger(LogStashEventJSONLayoutIT.class);
+    private static Logger logger = LogManager.getLogger(LogStashEventJSONLayoutIT.class);
 
-  @Test(enabled=false, dataProvider = "dp")
-  public void f(Integer n, String s) {
-  }
+    @Test(enabled = false, dataProvider = "dp")
+    public void f(Integer n, String s) {
+    }
 
-  @DataProvider
-  public Object[][] dp() {
-    return new Object[][] {
-      new Object[] { 1, "a" },
-      new Object[] { 2, "b" },
-    };
-  }
-  @BeforeTest
-  public void beforeTest() {
-  }
+    @DataProvider
+    public Object[][] dp() {
+        return new Object[][]{
+                new Object[]{1, "a"},
+                new Object[]{2, "b"},
+        };
+    }
 
-  @AfterTest
-  public void afterTest() {
-  }
+    @BeforeTest
+    public void beforeTest() {
+    }
 
-  
-  LogStashEventJSONLayout layout = LogStashEventJSONLayout.createLayout("UTF-8", "true", null, null, null, null, null);
+    @AfterTest
+    public void afterTest() {
+    }
 
-  //TODO make that token replacement for layout.foo() component
-  String simpleRoundTripJSON = "    {\n" + 
-  		"      \"@source\" : \"LogStashEventJSONLayout;"+ layout.getHostName() +";\",\n" + 
-  		"      \"@fields\" : {\n" + 
-  		"        \"source_address\" : \""+ layout.getLocalhostAddress() +"\",\n" + 
-  		"        \"source_application\" : null,\n" + 
-  		"        \"logger_name\" : \"net.logstash.logging.log4j2.core.layout.LogStashEventJSONLayoutIT\",\n" + 
-  		"        \"thread_name\" : \"Test worker\",\n" + 
-  		"        \"level\" : \"DEBUG\",\n" + 
-  		"        \"level_value\" : 5,\n" + 
-  		"        \"logger_fqcn\" : \"net.logstash.logging.log4j2.core.layout.LogStashEventJSONLayoutIT\",\n" + 
-  		"        \"nested_diagnostic_context\" : [ ],\n" + 
-  		"        \"message_type\" : \"SimpleMessage\",\n" + 
-  		"        \"message_formatted\" : \"Test Message\"\n" + 
-  		"      },\n" + 
-  		"      \"@source_host\" : \""+ layout.getHostName() +"\",\n" + 
-  		"      \"@timestamp\" : \"2013-06-30T09:22:12.322-07:00\",\n" + 
-  		"      \"@type\" : \"LogStashEventJSONLayout\"\n" + 
-  		"    }";
-  
-  ObjectMapper mapper = new ObjectMapper();
 
-  
-  @Test
-  public void BasicSimpleTest() throws JsonParseException, JsonMappingException, IOException{
-	  Message simpleMessage = new SimpleMessage("Test Message");
-	  LogEvent event = new Log4jLogEvent(logger.getName(),
-              null,
-              this.getClass().getCanonicalName(),
-              Level.DEBUG,
-              simpleMessage,
-              null);
-	  LogStashEventJSONLayout layout = LogStashEventJSONLayout.createLayout("UTF-8", "true", null, null, null, null, null);
-	  String layoutJSON = layout.toSerializable(event);
-	  
-	  ObjectNode expectedLayout = mapper.readValue(simpleRoundTripJSON, ObjectNode.class);
-	  ObjectNode resultLayout = mapper.readValue(layoutJSON, ObjectNode.class);
-	  
-	  //Remove timestamp (it's always goign to be different)
-	  expectedLayout.remove("@timestamp");
-	  resultLayout.remove("@timestamp");
-	  
-	  assertThat(resultLayout, equalTo(expectedLayout));	  
-  }
-  //TODO att order  
-  
-  //TODO test & demo local properties
-  
-  //TODO demo tags
+    LogStashEventJSONLayout layout = LogStashEventJSONLayout.createLayout("UTF-8", "true", null, null, null, null, null);
 
-  //TODO test & demo extra properties
+    //TODO make that token replacement for layout.foo() component
+    String simpleRoundTripJSON = "    {\n" +
+            "      \"@source\" : \"LogStashEventJSONLayout;" + layout.getHostName() + ";\",\n" +
+            "      \"@fields\" : {\n" +
+            "        \"source_address\" : \"" + layout.getLocalhostAddress() + "\",\n" +
+            "        \"source_application\" : null,\n" +
+            "        \"logger_name\" : \"net.logstash.logging.log4j2.core.layout.LogStashEventJSONLayoutIT\",\n" +
+            "        \"thread_name\" : \"Test worker\",\n" +
+            "        \"level\" : \"DEBUG\",\n" +
+            "        \"level_value\" : 5,\n" +
+            "        \"logger_fqcn\" : \"net.logstash.logging.log4j2.core.layout.LogStashEventJSONLayoutIT\",\n" +
+            "        \"nested_diagnostic_context\" : [ ],\n" +
+            "        \"message_type\" : \"SimpleMessage\",\n" +
+            "        \"message_formatted\" : \"Test Message\"\n" +
+            "      },\n" +
+            "      \"@source_host\" : \"" + layout.getHostName() + "\",\n" +
+            "      \"@timestamp\" : \"2013-06-30T09:22:12.322-07:00\",\n" +
+            "      \"@message\" : \"Test Message\",\n" +
+            "      \"@type\" : \"LogStashEventJSONLayout\"\n" +
+            "    }";
 
-  //TODO test throwables
-@SuppressWarnings({ "rawtypes", "unchecked" })
-@Test 
-public void TestThrowable() throws IOException {
-	  Message simpleMessage = new SimpleMessage("Test Message");
+    ObjectMapper mapper = new ObjectMapper();
 
-	  Throwable nestedThrowable = null;
-	  Throwable divideByZero = null;
 
-	  try {
-		@SuppressWarnings("unused")
-		int divide_by_zero = 12/0;	  
-	  } catch (RuntimeException r) {
-		  divideByZero = r;
-		  try {
-			  throw new IOException("Test IO Exception", r);
-		  }	catch (IOException e) {
-			  nestedThrowable = new RuntimeException("Runtime Exception", e);
-		  }
+    @Test
+    public void BasicSimpleTest() throws JsonParseException, JsonMappingException, IOException {
+        Message simpleMessage = new SimpleMessage("Test Message");
+        LogEvent event = new Log4jLogEvent(logger.getName(),
+                null,
+                this.getClass().getCanonicalName(),
+                Level.DEBUG,
+                simpleMessage,
+                null);
+        LogStashEventJSONLayout layout = LogStashEventJSONLayout.createLayout("UTF-8", "true", null, null, null, null, null);
+        String layoutJSON = layout.toSerializable(event);
 
-	  }
-	  
-	  LogEvent event = new Log4jLogEvent(logger.getName(),
-            null,
-            this.getClass().getCanonicalName(),
-            Level.DEBUG,
-            simpleMessage,
-            nestedThrowable);
-	  LogStashEventJSONLayout layout = LogStashEventJSONLayout.createLayout("UTF-8", "true", null, null, null, null, null);
-	  String layoutJSON = layout.toSerializable(event);
-	  
-	  ObjectNode resultLayout = mapper.readValue(layoutJSON, ObjectNode.class);
+        ObjectNode expectedLayout = mapper.readValue(simpleRoundTripJSON, ObjectNode.class);
+        ObjectNode resultLayout = mapper.readValue(layoutJSON, ObjectNode.class);
 
-	  System.out.println("--- resultLayout ---");
-	  System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(resultLayout));
-	  System.out.println("--- resultLayout ---");
-	  
-	  assertThat(resultLayout.has(LOGSTASH_EVENT_JSON_KEYS.FIELDS),is(true));
+        //Remove timestamp (it's always goign to be different)
+        expectedLayout.remove("@timestamp");
+        resultLayout.remove("@timestamp");
 
-	  assertThat(resultLayout.get(LOGSTASH_EVENT_JSON_KEYS.FIELDS).has("thrown"),is(true));
+        assertThat(resultLayout, equalTo(expectedLayout));
+    }
+    //TODO att order
 
-	  JsonNode throwableNode = resultLayout.get(LOGSTASH_EVENT_JSON_KEYS.FIELDS).get("thrown");
-	  
-	  System.out.println("--- throwableNODE ---");
-	  System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(throwableNode));
-	  System.out.println("--- throwableNODE ---");
-	  
-	 
-	  
-	  assertThat(IteratorUtils.toList(throwableNode.fieldNames()), 
-			  (Matcher) hasItems("exception_class","exception_message",
-					  "stacktrace","root_cause_throwable"));
+    //TODO test & demo local properties
 
-	  
-	  assertThat(throwableNode.get("exception_class").textValue(), equalTo("ThrowableProxy")); //BUG in LOG4j2!
-	  
-	  
-	  
-	  
-}
-  //TODO test variety of objects
-  
-  //TODO test all message types
- 
-  //TODO test markers
-  
-  //TODO demo expansion scenarios like audit log marker 
-  
-  //TODO demo parameter object like audit
-  
-  //TODO test context 
-  
+    //TODO demo tags
 
-  
-  /**
-   * This test requires logstash (installed manually) and makes assumptions
-   * about both configuration and operating system...
-   * 
-   * ... So, you probably ought not run it by default :)
-   */
-  @Test(groups="integration")
-  public void LogToLogStashTest() {
-	 System.out.println("&&&&&&&&&&&&&&&&&&&");
-	 System.out.println("&&&&&&&&&&&&&&&&&&&");
-	 System.out.println("&&&&&&&&&&&&&&&&&&&");
-	 System.out.println("&&&&&&&&&&&&&&&&&&&");
-	 logger.info("TEST IS WIRED");
-	  
-  }
-  
+    //TODO test & demo extra properties
+
+    //TODO test throwables
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @Test
+    public void TestThrowable() throws IOException {
+        Message simpleMessage = new SimpleMessage("Test Message");
+
+        Throwable nestedThrowable = null;
+        Throwable divideByZero = null;
+
+        try {
+            @SuppressWarnings("unused")
+            int divide_by_zero = 12 / 0;
+        } catch (RuntimeException r) {
+            divideByZero = r;
+            try {
+                throw new IOException("Test IO Exception", r);
+            } catch (IOException e) {
+                nestedThrowable = new RuntimeException("Runtime Exception", e);
+            }
+
+        }
+
+        LogEvent event = new Log4jLogEvent(logger.getName(),
+                null,
+                this.getClass().getCanonicalName(),
+                Level.DEBUG,
+                simpleMessage,
+                nestedThrowable);
+        LogStashEventJSONLayout layout = LogStashEventJSONLayout.createLayout("UTF-8", "true", null, null, null, null, null);
+        String layoutJSON = layout.toSerializable(event);
+
+        ObjectNode resultLayout = mapper.readValue(layoutJSON, ObjectNode.class);
+
+        System.out.println("--- resultLayout ---");
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(resultLayout));
+        System.out.println("--- resultLayout ---");
+
+        assertThat(resultLayout.has(LOGSTASH_EVENT_JSON_KEYS.FIELDS), is(true));
+
+        assertThat(resultLayout.get(LOGSTASH_EVENT_JSON_KEYS.FIELDS).has("thrown"), is(true));
+
+        JsonNode throwableNode = resultLayout.get(LOGSTASH_EVENT_JSON_KEYS.FIELDS).get("thrown");
+
+        System.out.println("--- throwableNODE ---");
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(throwableNode));
+        System.out.println("--- throwableNODE ---");
+
+
+        assertThat(IteratorUtils.toList(throwableNode.fieldNames()),
+                (Matcher) hasItems("exception_class", "exception_message",
+                        "stacktrace", "root_cause_throwable"));
+
+
+        assertThat(throwableNode.get("exception_class").textValue(), equalTo("RuntimeException"));
+
+
+    }
+    //TODO test variety of objects
+
+    //TODO test all message types
+
+    //TODO test markers
+
+    //TODO demo expansion scenarios like audit log marker
+
+    //TODO demo parameter object like audit
+
+    //TODO test context
+
+
+    /**
+     * This test requires logstash (installed manually) and makes assumptions
+     * about both configuration and operating system...
+     * <p/>
+     * ... So, you probably ought not run it by default :)
+     */
+    @Test(groups = "integration")
+    public void LogToLogStashTest() {
+        System.out.println("&&&&&&&&&&&&&&&&&&&");
+        System.out.println("&&&&&&&&&&&&&&&&&&&");
+        System.out.println("&&&&&&&&&&&&&&&&&&&");
+        System.out.println("&&&&&&&&&&&&&&&&&&&");
+        logger.info("TEST IS WIRED");
+
+    }
+
 }
